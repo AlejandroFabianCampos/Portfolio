ea4f0a6a0d66191d3ea6c1087f82eba74e7c64b9
AlejandroFabianCampos <alejandrofabiancampos2000@gmail.com>
2019-02-26 21:18:44 -0300

    Updates

---
+++ precache-manifest.56bd35160bb41f32e4adb6cbdccb18b5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "da95519d0e955668337c4013dc17a2ac",
+    "url": "/Portfolio/static/media/datCurriculumCut.da95519d.jpeg"
+  },
+  {
+    "revision": "e05ebb5afe3601fbee72",
+    "url": "/Portfolio/static/css/main.b19297cb.chunk.css"
+  },
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.591eb5be.js"
+  },
+  {
+    "revision": "4d45067e0e5d8f7c1b9b",
+    "url": "/Portfolio/static/js/2.4d45067e.chunk.js"
+  },
+  {
+    "revision": "445b78fbcd8dc37f83c722246fcafd5f",
+    "url": "/Portfolio/static/media/Kaboom.445b78fb.gif"
+  },
+  {
+    "revision": "2b2987a0d97c5d61ab31dab03d374303",
+    "url": "/Portfolio/static/media/DownArrow.2b2987a0.gif"
+  },
+  {
+    "revision": "e05ebb5afe3601fbee72",
+    "url": "/Portfolio/static/js/main.e05ebb5a.chunk.js"
+  },
+  {
+    "revision": "e7d3ce52560de32778200494d58b64b8",
+    "url": "/Portfolio/static/media/ResponsiveIcon.e7d3ce52.png"
+  },
+  {
+    "revision": "826555203a8648d4404d4ce939fc4513",
+    "url": "/Portfolio/static/media/ReactIcon.82655520.png"
+  },
+  {
+    "revision": "d926da38befa8565aaa9feb7e37b1ded",
+    "url": "/Portfolio/static/media/FastIcon.d926da38.png"
+  },
+  {
+    "revision": "a76ed0d6b7c30859a4de82d5f9ee4ebc",
+    "url": "/Portfolio/static/media/LinkedinIcon.a76ed0d6.png"
+  },
+  {
+    "revision": "cad87121c18ff8ae1df40d72a89ab840",
+    "url": "/Portfolio/static/media/GithubIcon.cad87121.png"
+  },
+  {
+    "revision": "46d9e0ad2bdec155c99672dc165659b8",
+    "url": "/Portfolio/index.html"
+  }
+];
